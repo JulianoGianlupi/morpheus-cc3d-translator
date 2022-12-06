bafb033aec52206ca04275c147ca0d8df2ba9878
JulianoGianlupi <julianoferrarigianlupi@gmail.com>
2022-12-06 18:06:18 -0300

    neighbor loop gen done

--- translator.py
+++ translator.py
@@ -35,10 +35,62 @@ def make_globals_str(ccglo):
     return st
 
 
+def get_neighbors_loops(mdict):
+    mctype = mdict["CellTypes"]['CellType']
+    ccloops = {}
+    for typedict in mctype:
+        if 'NeighborhoodReporter' in typedict:
+            ccloops[typedict['@name']] = typedict['NeighborhoodReporter']
+            # if len(typedict['NeighborhoodReporter']):
+            #     for report in typedict['NeighborhoodReporter']:
+
+    return ccloops
+
+
+def make_cc3d_neighbors_loops(ccloops, ccglo=None):
+    mega_str = ""
+    for ctype, loops in ccloops.items():
+        ctype_loop = f"\n\t\tfor cell in self.cell_list_by_type(self.{ctype.upper()}):\n"
+        # for var, value in ccglo.items():
+        #     ctype_loop += f"\t\t\t{var} = {value}\n"
+        if type(loops) == list:
+            ifs = []
+            for loop in loops:
+                ctype_2 = loop['Input']['@value'].split(".")[-2]
+                ctype_loop += f"\t\t\t{loop['Output']['@symbol-ref']} = 0\n"
+                if_neigh = f"\t\t\t\tif neighbor and neighbor.type == self.{ctype_2.upper()}:\n"
+                if loop['Input']['@scaling'] == "length":
+                    if_neigh += f"\t\t\t\t\t{loop['Output']['@symbol-ref']} += common_surface_area\n"
+                else:
+                    if_neigh += f"\t\t\t\t\t{loop['Output']['@symbol-ref']} += 1\n"
+
+                ifs.append(if_neigh)
+        else:
+            ifs = []
+            ctype_2 = loops['Input']['@value'].split(".")[-2]
+            ctype_loop += f"\t\t\t{loops['Output']['@symbol-ref']} = 0\n"
+            if_neigh = f"\t\t\t\tif neighbor and neighbor.type == self.{ctype_2.upper()}:\n"
+            if loops['Input']['@scaling'] == "length":
+                if_neigh += f"\t\t\t\t\t{loops['Output']['@symbol-ref']} += common_surface_area\n"
+            else:
+                if_neigh += f"\t\t\t\t\t{loops['Output']['@symbol-ref']} += 1\n"
+
+            ifs.append(if_neigh)
+
+        ctype_loop += f"\t\t\tfor neighbor, common_surface_area in self.get_cell_neighbor_data_list(cell):\n"
+        for iif in ifs:
+            ctype_loop += iif
+        mega_str += ctype_loop
+
+    return mega_str
+
+
 def main(file_to_translate):
     mdict = get_morpheus_as_dict(file_to_translate)
     cc3d_globals = get_globals(mdict)
     globals_string = make_globals_str(cc3d_globals)
+    neigh_loop = get_neighbors_loops(mdict)
+    cc3d_neigh_loops = make_cc3d_neighbors_loops(neigh_loop)
 
 
 if __name__ == "__main__":
@@ -46,4 +98,6 @@ if __name__ == "__main__":
     mdict = get_morpheus_as_dict(example)
     cc3d_globals = get_globals(mdict)
     globals_string = make_globals_str(cc3d_globals)
+    neigh_loop = get_neighbors_loops(mdict)
+    cc3d_neigh_loops = make_cc3d_neighbors_loops(neigh_loop)
     print("end")
