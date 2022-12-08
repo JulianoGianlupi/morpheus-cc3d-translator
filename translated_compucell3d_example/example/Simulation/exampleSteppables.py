1443c68729bec19db34def6fc519a3a39b6a06a8
JulianoGianlupi <julianoferrarigianlupi@gmail.com>
2022-12-08 11:08:49 -0300

    copyright

--- translated_compucell3d_example/example/Simulation/exampleSteppables.py
+++ translated_compucell3d_example/example/Simulation/exampleSteppables.py
@@ -5,6 +5,23 @@ import numpy as np
 
 
 
+'''
+Copyright (C) 2022, Juliano Ferrari Gianlupi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
 global boundary
 boundary = 0.0
 
@@ -26,8 +43,8 @@ class exampleSteppable(SteppableBasePy):
 		"""
 		Called before MCS=0 while building the initial simulation
 		"""
-		# self.plot_win = self.add_new_plot_window(title='Inter-type common contact area', x_axis_title='MonteCarlo Step (MCS)',  y_axis_title='Area', x_scale_type='linear', y_scale_type='linear', grid=True)
-		# self.plot_win.add_plot('Area', style='Lines', color='red', size=5)
+		self.plot_win = self.add_new_plot_window(title='Inter-type common contact area', x_axis_title='MonteCarlo Step (MCS)',  y_axis_title='Area', x_scale_type='linear', y_scale_type='linear', grid=True)
+		self.plot_win.add_plot('Area', style='Lines', color='red', size=5)
 
 	def step(self, mcs):
 		"""
@@ -63,7 +80,7 @@ class exampleSteppable(SteppableBasePy):
 					b2 += common_surface_area
 				cell.dict['b2']=b2
 
-		# self.plot_win.add_data_point('Area', mcs, boundary)
+		self.plot_win.add_data_point('Area', mcs, boundary)
 
 	def finish(self):
 		"""
--- translator.py
+++ translator.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2022 Juliano Ferrari Gianlupi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import xmltodict as x2d
 import steppable_gen_functions as sgf
 
@@ -109,6 +133,31 @@ if __name__ == "__main__":
     example = r"./morpheus_examples/CellSorting_2D.xml"
     mdict = get_morpheus_as_dict(example)
     cc3d_globals = get_globals(mdict)
+    copy = """
+'''
+MIT License
+
+Copyright (c) 2022 Juliano Ferrari Gianlupi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+"""
     globals_string = make_globals_str(cc3d_globals)
     neigh_loop = get_neighbors_loops(mdict)
     cc3d_neigh_loops = make_cc3d_neighbors_loops(neigh_loop)
@@ -121,7 +170,7 @@ if __name__ == "__main__":
 
     _extra_step = "\n\t\tself.plot_win.add_data_point('Area', mcs, boundary)"
     _extra_step = cc3d_neigh_loops + _extra_step
-    steppable_string = sgf.generate_steppable("example", 1, False, additional_imports=globals_string,
+    steppable_string = sgf.generate_steppable("example", 1, False, additional_imports=copy+globals_string,
                                               additional_step=_extra_step, additional_init=_extra_init,
                                               additional_start=_extra_start)
 
